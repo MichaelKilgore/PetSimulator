017b81e8f64fb81daa48d1300dcbf2f098ac5e01
David <drahenshaw@gmail.com>
2019-02-23 12:25:21 -0800

    Animation Class

---
+++ PetSimulatorSFML/PetSimulator/Animation.cpp
@@ -0,0 +1,44 @@
+#include "Animation.h"
+
+Animation::Animation(){}
+
+Animation::Animation(int posX, int posY, int imgWidth, int imgHeight, std::string fileName, int frameAdjust)
+{
+	texture_.loadFromFile(fileName);
+
+	for (int i = 0; i < numFrames_ - frameAdjust; ++i)
+	{
+		frames_[i] = { posX + i * imgWidth, posY, imgWidth, imgHeight };
+	}
+}
+
+void Animation::ApplyToSprite(sf::Sprite & sprite) const
+{
+	sprite.setTexture(texture_);
+	sprite.setTextureRect(frames_[iFrame_]);
+}
+
+void Animation::Update(float dT, int frameAdjust)
+{
+	time_ += dT;
+
+	while (time_ >= holdTime_)
+	{
+		time_ -= holdTime_;
+		Advance(frameAdjust);
+	}
+}
+
+void Animation::SetAnimationFrames(int frameAdjust)
+{
+	frames_[numFrames_ - frameAdjust];
+}
+
+void Animation::Advance(int frameAdjust)
+{
+	if (++iFrame_ >= numFrames_ - frameAdjust)
+	{
+		iFrame_ = 0;
+	}
+}
+
---
+++ PetSimulatorSFML/PetSimulator/Animation.h
@@ -0,0 +1,28 @@
+#ifndef PETSIMULATOR_ANIMATION
+#define PETSIMULATOR_ANIMATION
+
+#include <SFML/Graphics.hpp>
+
+class Animation
+{
+public:
+	Animation();
+	Animation(int posX, int posY, int imgWidth, int imgHeight, std::string fileName, int frameAdjust);
+
+	void ApplyToSprite(sf::Sprite &sprite) const;
+	void Update(float dT, int frameAdjust);
+	void SetAnimationFrames(int frameAdjust);
+
+private:
+	void Advance(int frameAdjust);
+
+private:
+	static constexpr int numFrames_ = 3;
+	static constexpr float holdTime_ = 0.1f;
+	sf::Texture texture_;
+	sf::IntRect frames_[numFrames_];
+	int iFrame_;
+	float time_;
+};
+
+#endif
