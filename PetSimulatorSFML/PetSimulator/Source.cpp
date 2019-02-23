07690ebd429b3b993abe9dc7bbfeb26ea1c3161c
David <drahenshaw@gmail.com>
2019-02-23 11:21:53 -0800

    Initial SFML Window Setup

--- PetSimulatorSFML/PetSimulator/Source.cpp
+++ PetSimulatorSFML/PetSimulator/Source.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(void)
 {
-	system("pause");
-	return 0;
+	constexpr int SCREEN_WIDTH = 800;
+	constexpr int SCREEN_HEIGHT = 600; 
+
+	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 }
